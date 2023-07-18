1fb1bc83d639e5e3ce91624f58b2781fe0925318
Bino-GG <abanoub.g.guirguis@gmail.com>
2023-07-18 12:15:53 -0700

    main projects

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,25 +9,34 @@
 
 void print_to_98(int n)
 {
-	int i, j;
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
 
-	if(n <== 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d", i);
-			else if (i == 98)
-				printf("%d\n", i);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 		}
-	} else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if(j != 98)
-				printf("%d", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
+		printf("\n");
 	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else
+
+		printf("98");
+	return (98);
 }
