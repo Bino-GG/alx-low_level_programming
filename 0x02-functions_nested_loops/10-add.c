8aef0b074229c483ff9697947502f3703aa21e0e
Bino-GG <abanoub.g.guirguis@gmail.com>
2023-07-18 11:55:00 -0700

    main projects

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and return the result
+ * 0a: int to be added to b
+ * 0b: int to be added to a
+ * Return: sun of a and b
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * 0n: the int to check
+ * Return: 1 and prints + if n is greater then zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less then zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
