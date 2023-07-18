f2968656bd02a9b61757cd5e18a493e4ee1c09f2
Bino-GG <abanoub.g.guirguis@gmail.com>
2023-07-18 11:02:57 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_alphabet - uilizes on the _putchar function to print
- * a - z
+ * print_alphabet - print all alphabet in lowercase
 */
 
 void print_alphabet(void)
 {
-	int ch;
+	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
-	-putchar('\n');
+
+	_putchar('\n');
 }
